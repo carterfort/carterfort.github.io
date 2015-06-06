a8264b649e45cb37b11ae06c186420195dc9565a
Carter Fort <carter.fort@gmail.com>
2015-06-06 11:15:00 -0400

    Testing scripts

---
+++ tesla/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+				<h1><?php echo "Does this run scripts?"; ?></h1>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+</html>
